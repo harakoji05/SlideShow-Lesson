05c37bd62d62ead9ff1f4dff1168ba095192da31
harakoji05 <harakoji.0514@gmail.com>
2020-05-28 11:37:56 +0900

    first commit

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+$(".theTarget").skippr({
+
+    transition : 'slide',
+
+    speed : 1000,
+
+    easing : 'easeOutQuart',
+
+    navType : 'bubble',
+
+    childrenElementType : 'div',
+
+    arrows : true,
+
+    autoPlay : false,
+
+    autoPlayDuration : 5000,
+
+    keyboardOnAlways : true,
+
+    hidePrevious : false
+});
